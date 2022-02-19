33b6a5c441bb8261d2867e92bc1821c1b3e1c190
osw0124 <osw0124@daum.net>
2022-02-19 18:29:53 +0900

    미들웨어 검색 방법 변경, 후기수정 오타 수정

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -4,7 +4,7 @@ const jwtSecret = process.env.JWT_SECRET;
 
 module.exports = (req, res, next) => {
     const {authorization} = req.headers;
-    // console.log(authorization);
+    // console.log('authorization', authorization);
     if (!authorization){
         res.send({
             ok: false,
@@ -23,12 +23,11 @@ module.exports = (req, res, next) => {
         return;
     }
 
-    try {
-        const {userId} = jwt.verify(tokenValue, `${jwtSecret}`);
-        console.log('userId:', userId);
-        users.findById(userId).exec().then((user) => {
+    try {        
+        const {user_id} = jwt.verify(tokenValue, `${jwtSecret}`);
+        users.findOne({user_id}).exec().then((user) => {        
             res.locals.user = user;
-            console.log(res.locals.user);
+            // console.log(res.locals.user);
             next();
         });
     } catch (error) {
