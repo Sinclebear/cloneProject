4f9799e34cee685083b7bae07d8b708b0afed09b
osw0124 <osw0124@daum.net>
2022-02-19 16:57:07 +0900

    후기 수정 api 추가

--- routes/comments.js
+++ routes/comments.js
@@ -11,10 +11,32 @@ router.post('/comment/save/write', authmiddlewares, async (req, res) => {
     
     await Comments.create({user_nick: user.user_nick, comment, home_rate, home_name, createdAt: new Date()});
 
-    res.json({
+    res.send({
         success: '후기 작성이 완료되었습니다.'
     });
 });
+
+//후기 수정
+router.patch('/comment/:commentId', authmiddlewares, (async, res) => {
+    const {commentId} =req.params;
+    const {comment, home_rate} = req.body;
+
+    await Comments.findByIdAndUpdate({_id: commentId}, {comment, home_rate});
+    res.send({
+
+    });
+});
+
+
+//후기 삭제
+router.delete('/comment/:commentId', authmiddlewares, async (req, res) => {
+    const {commentId} = req.params;
+    await Comments.deleteOne({_id: commentId});
+    res.send({
+        success: '후기 삭제가 완료되었습니다.'
+    });
+});
+
 
 
 module.exports = router;
