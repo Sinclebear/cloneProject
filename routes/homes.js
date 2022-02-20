a40e1e7b729c2cfc7e25b85c949050406dd06546
G-moog <92852591+G-moog@users.noreply.github.com>
2022-02-20 21:07:17 +0900

    /main 콘솔로그 주석화

--- routes/homes.js
+++ routes/homes.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const Homes = require('../models/homeSchema');
 
 router.get("/main", async (req, res) => {
-  console.log("음메");
+  // console.log("음메");
   const homes = await Homes.find().exec();
-  console.log("냐옹");
+  // console.log("냐옹");
   res.send({ homes });
   console.log('숙소 목록을 보냈습니다.')
 });
