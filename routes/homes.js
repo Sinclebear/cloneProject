961fc47cc8bb453ad6b5cb6292da448e565eb153
osw0124 <osw0124@daum.net>
2022-02-21 17:43:01 +0900

    숙소 등록 api 추가

--- routes/homes.js
+++ routes/homes.js
@@ -12,19 +12,25 @@ router.get("/main", async (req, res) => {
   console.log('숙소 목록을 보냈습니다.')
 });
 
+//숙소 등록
 router.post('/hosting', authmiddlewares, async (req, res) => {
-  const { user } = res.locals;
-  console.log(user);
-  const {home_name, category, address, image_url, introduce, price, convenience, distance, availableDate, } = req.body;
+  const { host_cert } = res.locals.user
+  console.log(host_cert);
+
+  if(host_cert === false) {
+    res.send({
+      fail: '호스트 권한이 없는 계정입니다.'
+    });
+    return;
+  }  
+
+  const {home_name, category, address, image_url, introduce, price, convenience, distance, availableDate} = req.body;
   
-  await Homes.create({home_name, category, address, image_url, introduce, price, convenience, distance, availableDate, createdAt: new Date(), updatedAt: new Date()});
+  await Homes.create({home_name, category, address, image_url, introduce, price, convenience, distance, availableDate, host_name: res.locals.user.user_id , createdAt: new Date(), updatedAt: new Date()});
   
   res.send({
       success: '숙소 등록이 완료되었습니다.'
   });
 });
-
-
-
 
 module.exports = router;
