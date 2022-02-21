48db80f196b51821c962b462749ab5fc84788628
osw0124 <osw0124@daum.net>
2022-02-21 17:19:45 +0900

    호스트 해제 추가

--- routes/host.js
+++ routes/host.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 require('dotenv').config();
-const Homes = require('../models/homeSchema');
 const Users = require('../models/userSchema');
 const authmiddlewares = require('../middlewares/auth-middleware');
 
@@ -21,19 +20,20 @@ router.post('/host/regist', authmiddlewares, async (req, res) => {
         });
 });
 
+//호스트 해제
+router.post('/host/delete', authmiddlewares, async (req, res) => {
+    const {user_id} = res.locals.user;
+        
+    if(res.locals.user.host_cert === false) {
+        res.send({                      
+            fail: "호스트로 등록되어있지 않은 계정입니다.."
+        });
+        return;
+    }
+    await Users.findOneAndUpdate({user_id: user_id}, {host_cert: false});
+        res.send({
+            success: "호스트 해제 성공"
+        });
+});
+
 module.exports = router;
-
-
-
-// //후기 작성
-// router.post('/comment/save/write/:homeId', authmiddlewares, async (req, res) => {
-//     const { user } = res.locals;
-//     console.log(user);
-//     const {comment, home_rate, home_name} = req.body;
-    
-//     await Comments.create({user_nick: user.user_nick, comment, home_rate, home_name, home_id: req.params.homeId, createdAt: new Date()});
-
-//     res.send({
-//         success: '후기 작성이 완료되었습니다.'
-//     });
-// });
