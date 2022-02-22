d802fbacdeb6eeb1a8615d4c9b2d646c5599dbd2
osw0124 <osw0124@daum.net>
2022-02-22 17:57:12 +0900

    정렬

--- routes/host.js
+++ routes/host.js
@@ -6,34 +6,34 @@ const authmiddlewares = require('../middlewares/auth-middleware');
 
 //호스트 등록
 router.put('/host/regist', authmiddlewares, async (req, res) => {
-    const {user_id} = res.locals.user;
-        
-    if(res.locals.user.host_cert === true) {
-        res.send({                      
+    const { user_id } = res.locals.user;
+
+    if (res.locals.user.host_cert === true) {
+        res.send({
             fail: "이미 호스트로 등록되어있는 계정입니다."
         });
         return;
     }
-    await Users.findOneAndUpdate({user_id: user_id}, {host_cert: true});
-        res.send({
-            success: "호스트 등록 성공"
-        });
+    await Users.findOneAndUpdate({ user_id: user_id }, { host_cert: true });
+    res.send({
+        success: "호스트 등록 성공"
+    });
 });
 
 //호스트 해제
 router.put('/host/delete', authmiddlewares, async (req, res) => {
-    const {user_id} = res.locals.user;
-        
-    if(res.locals.user.host_cert === false) {
-        res.send({                      
+    const { user_id } = res.locals.user;
+
+    if (res.locals.user.host_cert === false) {
+        res.send({
             fail: "호스트로 등록되어있지 않은 계정입니다.."
         });
         return;
     }
-    await Users.findOneAndUpdate({user_id: user_id}, {host_cert: false});
-        res.send({
-            success: "호스트 해제 성공"
-        });
+    await Users.findOneAndUpdate({ user_id: user_id }, { host_cert: false });
+    res.send({
+        success: "호스트 해제 성공"
+    });
 });
 
 module.exports = router;
