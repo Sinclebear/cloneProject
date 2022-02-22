7ecc46d8b529aa8509ca5ab0684daf5aa366b78b
G-moog <92852591+G-moog@users.noreply.github.com>
2022-02-22 18:15:17 +0900

    response에 status code 삭제

--- routes/users.js
+++ routes/users.js
@@ -10,12 +10,12 @@ require('dotenv').config();
 const postUsersSchemas = Joi.object({
   user_id: Joi.string()
     .required()
-    .pattern(new RegExp('^[a-zA-Z0-9가-힣]{4,16}$')),
+    .pattern(new RegExp('^[a-zA-Z0-9]{4,16}$')),
   user_nick: Joi.string().required().min(4).max(10),
   user_pwd: Joi.string().required()
-    .pattern(new RegExp('^[a-zA-Z0-9가-힣]{4,16}$')),
+    .pattern(new RegExp('^[a-zA-Z0-9]{4,16}$')),
   confirmPassword: Joi.string().required()
-    .pattern(new RegExp('^[a-zA-Z0-9가-힣]{4,16}$')),
+    .pattern(new RegExp('^[a-zA-Z0-9]{4,16}$')),
 });
 
 // 회원가입구현
@@ -24,34 +24,36 @@ router.post('/signup', async (req, res) => {
     console.log(req.body);
     const { user_id, user_nick, user_pwd, confirmPassword } = await postUsersSchemas.validateAsync(req.body);
     if (user_pwd !== confirmPassword) {
-      res.status(400).send({
-        errorMessage: '비밀번호를 확인해주세요.',
+      res.send({
+        fail: '비밀번호를 확인해주세요.',
       });
       return;
     }
     if (user_id === user_pwd) {
-      res.status(400).send({
-        errorMessage: '아이디와 비밀번호가 동일하면 안됩니다.',
+      res.send({
+        fail: '아이디와 비밀번호가 동일하면 안됩니다.',
       });
       return;
     }
 
     const existUsers = await User.find({ user_id });
     if (existUsers.length) {
-      res.status(400).send({
-        errorMessage: '이미 가입된 아이디입니다.',
+      res.send({
+        fail: '이미 가입된 아이디입니다.',
       });
       return;
     }
     const user = new User({ user_id, user_pwd, user_nick, host_cert: false });
     await user.save();
 
-    res.status(201).send({});
+    res.send({
+      success: "회원가입이 완료되었습니다."
+    });
   } catch (err) {
     // console.log(err.details[0].message)
     // console.log(err);
-    res.status(400).send({
-      errorMessage: '요청한 형식이 올바르지 않습니다.', 
+    res.send({
+      fail: '요청한 형식이 올바르지 않습니다.',
     });
   }
 });
@@ -65,31 +67,19 @@ const postAuthSchemas = Joi.object({
 
 // 로그인 구현
 router.post('/login', async (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
   try {
     const { user_id, user_pwd } = await postAuthSchemas.validateAsync(req.body);
-    // console.log("여기까진 오나?");
     const user = await User.findOne({ user_id }).exec();
-    // console.log("검색은 하나??");
-    console.log(user);
     if (!user) {
-      res.status(400).send({
-        errorMessage: '닉네임 또는 패스워드가 잘못됐습니다.',
+      res.send({
+        fail: '닉네임 또는 패스워드가 잘못됐습니다.',
       });
       return;
     }
-    // console.log("1차 검증");
-    // user.comparePassword(req.body.user_pwd, (err, isMatch) => {
-    //   if (!isMatch) {
-    //     return res.status(400).send({
-    //       errorMessage: '닉네임 또는 패스워드가 잘못됐습니다.',
-    //     });
-    //   }
-    // });
-    // console.log("아디 비번은 맞고");
     
     const token = jwt.sign(
-      { user_id: user.user_id ,user_nick: user.user_nick},
+      { user_id: user.user_id, user_nick: user.user_nick },
       process.env.JWT_SECRET
     );
     // console.log("토큰 내부값은?", token);
@@ -98,8 +88,8 @@ router.post('/login', async (req, res) => {
     });
   } catch (error) {
     // console.log(error);
-    res.status(400).send({
-      errorMessage: '요청한 형식이 올바르지 않습니다.',
+    res.send({
+      fail: '아이디 또는 비밀번호를 확인해주세요.',
     });
   }
 });
@@ -107,58 +97,70 @@ router.post('/login', async (req, res) => {
 // 사용자인증 미들웨어
 router.get('/auth', authMiddlleware, async (req, res) => {
   const { user } = res.locals;
-  res.send({
-    user: {
-      user_id: user.user_id,
-      user_nick: user.user_nick
-    },
-  });
+  if (user) {
+    res.send({
+      success: success,
+
+      user: {
+        user_id: user.user_id,
+        user_nick: user.user_nick
+
+      }
+    })
+  } else{
+    res.send({
+      fail: "로그인후 이용하세요",
+    })
+  }
+
+
+
 });
 
 router.post('/signup/checkid', async (req, res) => {
   // console.log("아이디 중복체크 api시작");
   const input_id = req.body.user_id;
   // console.log("input_id값은  ", input_id);
-  
+
   const existUsers = await User.find({ user_id: input_id });
 
   // console.log("existUser: ", existUsers[0].user_id);
   // console.log("existUser: ", existUsers);
-  
+
   if (existUsers.length) {
-      res.send({
-        success: '이미 가입된 아이디입니다.',
-      });
-      return;
-    }
-    
-    res.send({ 
-      fail:"사용가능한 아이디입니다.", 
+    res.send({
+      success: '이미 가입된 아이디입니다.',
     });
-  } 
+    return;
+  }
+
+  res.send({
+    fail: "사용가능한 아이디입니다.",
+  });
+}
 );
 
 router.post('/signup/checknick', async (req, res) => {
   // console.log("닉네임 중복체크 api시작");
   const input_nick = req.body.user_nick;
   // console.log("input_nick값은  ", input_nick);
-  
+
   const existUsers = await User.find({ user_nick: input_nick });
 
   // console.log("existUser: ", existUsers[0].user_nick);
   // console.log("existUser: ", existUsers);
-  
+
   if (existUsers.length) {
-      res.send({
-        success: '이미 사용중인 닉네임입니다.',
-      });
-      return;
-    }
-    
-    res.send({ 
-      fail:"사용가능한 닉네임입니다." 
+    res.send({
+      success: '이미 사용중인 닉네임입니다.',
     });
-  } 
+    return;
+  }
+
+  res.send({
+    fail: "사용가능한 닉네임입니다."
+  });
+}
 );
 
 
