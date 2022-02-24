3e44d9a3a7b7cd1bdd4920cca94958a1e8db8bdc
osw0124 <osw0124@daum.net>
2022-02-24 16:16:09 +0900

    msg 통일

--- routes/users.js
+++ routes/users.js
@@ -73,7 +73,7 @@ router.post('/login', async (req, res) => {
     const user = await User.findOne({ user_id, user_pwd }).exec();
     if (!user) {
       res.send({
-        fail: '닉네임 또는 패스워드가 잘못됐습니다.',
+        msg: '닉네임 또는 패스워드가 잘못됐습니다.',
       });
       return;
     }
@@ -86,6 +86,8 @@ router.post('/login', async (req, res) => {
     res.send({
       token,
 
+      msg: "로그인에 성공했습니다.",
+
       user: {
         user_id: user.user_id,
         user_nick: user.user_nick
@@ -93,7 +95,7 @@ router.post('/login', async (req, res) => {
   } catch (error) {
     // console.log(error);
     res.send({
-      fail: '아이디 또는 비밀번호를 확인해주세요.',
+      msg: '아이디 또는 비밀번호를 확인해주세요.',
     });
   }
 });
